5ca851d6fc2805faadc159b5b9dca21b2b3906b8
Matheus Oliveira <msouza.os@hotmail.com>
2024-10-29 22:07:58 -0300

    update: Testing multi head attention class.

--- tests/models/test_attention.py
+++ tests/models/test_attention.py
@@ -5,7 +5,7 @@ from unittest import TestCase
 
 import torch
 
-from models.decoder import Attention
+from models.decoder import Attention, MultiHeadAttention
 
 
 # Classes.
@@ -100,3 +100,64 @@ class TestAttetion(TestCase):
         self.assertTrue(assert_mean)
         assert_var = torch.isclose(attention.score.var(), torch.tensor(0.1), atol=1e-1)
         self.assertTrue(assert_var)
+
+
+    def test_attention_shape(self):
+        """
+        Tests the shape of the attention matrix.
+        """
+        # Create an Attention object.
+        attention = Attention(
+            base_embedding_dim=768,
+            embedding_dim=768
+        )
+
+        # Mock tensors.
+        query = torch.randn(1, 196, 768)
+        key = torch.randn(1, 196, 768)
+        value = torch.randn(1, 196, 768)
+
+        # Get the attention output.
+        with torch.no_grad():
+            _ = attention(query, key, value)
+
+        # Check the attention shape.
+        self.assertEqual(attention.attention.size(), (1, 196, 196))
+
+
+class TestMultiHeadAttention(TestCase):
+
+
+    # Tests.
+    def test_init(self):
+        """
+        Tests the __init__ method.
+        """
+        # Create a MultiHeadAttention object.
+        MultiHeadAttention(
+            embedding_dim=768,
+            num_heads=8
+        )
+
+
+    def test_multi_head_attention(self):
+        """
+        Tests the forward method.
+        """
+        # Create a MultiHeadAttention object.
+        attention = MultiHeadAttention(
+            embedding_dim=768,
+            num_heads=8
+        )
+
+        # Mock tensors.
+        query = torch.randn(1, 196, 768)
+        key = torch.randn(1, 196, 768)
+        value = torch.randn(1, 196, 768)
+
+        # Get the attention output.
+        with torch.no_grad():
+            output = attention(query, key, value)
+
+        # Check the size of the output.
+        self.assertEqual(output.size(), (1, 196, 768))
