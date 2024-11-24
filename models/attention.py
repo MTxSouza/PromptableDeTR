5b015d8de6ff2e7d695f4da49247c4fbf40c263c
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2024-11-24 17:54:32 -0300

    update: Change implementation of deformable attention and clean the base attention mechanism.

--- models/attention.py
+++ models/attention.py
@@ -3,6 +3,7 @@ Main module that contains the attention classes to compose both the text and ima
 """
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 # Classes.
@@ -10,17 +11,14 @@ class Attention(nn.Module):
 
 
     # Special methods.
-    def __init__(self, in_embedding_dim, out_embedding_dim, attention_size = None):
+    def __init__(self, in_embedding_dim, out_embedding_dim):
         """
         Initializes the Attention class that computes the attention weights with the normal 
-        dot-product mechanism. Optionally, it can also compute the attention weights with a 
-        deformable mechanism that uses an offset tensor from image embeddings used to deform the 
-        attention weights on image based on both text and image embeddings.
+        dot-product mechanism.
 
         Args:
             in_embedding_dim (int): The input embedding dimension.
             out_embedding_dim (int): The output embedding dimension.
-            attention_size (int): The size of the attention tensor. (Default: None)
         """
         super().__init__()
 
@@ -29,13 +27,8 @@ class Attention(nn.Module):
         self.__key = nn.Linear(in_features=in_embedding_dim, out_features=out_embedding_dim, bias=False)
         self.__value = nn.Linear(in_features=in_embedding_dim, out_features=out_embedding_dim, bias=False)
 
-        self.__offset = None
-        if attention_size is not None:
-            self.__offset = nn.Linear(in_features=in_embedding_dim, out_features=attention_size, bias=False)
-
         # Attributes.
         self.__attention = None
-        self.__offset_attention = None
 
 
     # Properties.
@@ -50,29 +43,15 @@ class Attention(nn.Module):
         return self.__attention
 
 
-    @property
-    def offset_attention(self):
-        """
-        Get the offset attention matrix computed by the forward pass.
-
-        Returns:
-            torch.Tensor: The offset attention matrix.
-        """
-        return self.__offset_attention
-
-
     # Methods.
-    def forward(self, query, key, value, offset = None):
+    def forward(self, query, key, value):
         """
-        Forward pass of the attention mechanism. Make sure to enable the offset layer defining 
-        the `attention_size` parameter in the constructor, otherwise it will raise an error if 
-        you pass the `offset` parameter during the forward pass.
+        Forward pass of the attention mechanism.
 
         Args:
-            query (torch.Tensor): The query tensor.
-            key (torch.Tensor): The key tensor.
-            value (torch.Tensor): The value tensor.
-            offset (torch.Tensor): The offset tensor. (Default: None)
+            query (torch.Tensor): The query tensor, usually the text embeddings.
+            key (torch.Tensor): The key tensor, usually the image embeddings.
+            value (torch.Tensor): The value tensor, usually the image embeddings.
 
         Returns:
             torch.Tensor: The output tensor.
@@ -80,22 +59,8 @@ class Attention(nn.Module):
         # Compute the attention weights.
         attention_weights = self.__get_attention_weights(query=query, key=key)
 
-        # Try to compute the offset tensor and aggregate to 
-        # the attention weights if it is defined.
-        if offset is not None:
-            assert self.__offset is not None, "The offset layer is not defined."
-            o = self.__offset(offset)
-
-            # Aggregate the offset.
-            assert attention_weights.size() == o.size(), "The attention weights and offset tensor must have the same size."
-            attention_weights = attention_weights + o
-
-            # Save the offset attention matrix.
-            self.__offset_attention = attention_weights.clone()
-
         # Compute the output tensor.
-        v = self.__value(value)
-        output = torch.matmul(attention_weights, v)
+        output = self.__get_output_embedding(attention_weights=attention_weights, value=value)
 
         return output
 
@@ -106,8 +71,8 @@ class Attention(nn.Module):
         Computes the attention weights given the query and key tensors.
 
         Args:
-            query (torch.Tensor): The query tensor.
-            key (torch.Tensor): The key tensor.
+            query (torch.Tensor): The query tensor, usually the text embeddings.
+            key (torch.Tensor): The key tensor, usually the image embeddings.
 
         Returns:
             torch.Tensor: The attention weights.
@@ -125,6 +90,187 @@ class Attention(nn.Module):
         self.__attention = attention_weights.clone()
 
         return attention_weights
+
+
+    def __get_output_embedding(self, attention_weights, value):
+        """
+        Computes the output embedding given the attention weights and value tensor.
+
+        Args:
+            attention_weights (torch.Tensor): The attention weights.
+            value (torch.Tensor): The value tensor, usually the image embeddings.
+
+        Returns:
+            torch.Tensor: The output embedding.
+        """
+        # Compute the output tensor.
+        v = self.__value(value)
+        output = torch.matmul(attention_weights, v)
+
+        return output
+
+
+class DeformableAttention(nn.Module):
+
+
+    # Special methods.
+    def __init__(self, in_embedding_dim, out_embedding_dim, num_points, patch_size, num_levels = 1):
+        """
+        Initializes the DeformableAttention class that computes the attention weights with a 
+        deformable mechanism that uses an offset tensor from image embeddings used to deform the 
+        attention weights on image based on both text and image embeddings.
+
+        Args:
+            in_embedding_dim (int): The input embedding dimension.
+            out_embedding_dim (int): The output embedding dimension.
+            num_points (int): The number of points to deform the attention weights.
+            patch_size (int): The size of the patch used to compute the reference points.
+            num_levels (int): The number of levels to deform the attention weights. (Default: 1)
+        """
+        super().__init__()
+
+        # Layers.
+        self.__offset = nn.Linear(in_features=in_embedding_dim, out_features=num_levels * num_points * 2, bias=False)
+        self.__att_weight = nn.Linear(in_features=in_embedding_dim, out_features=num_levels * num_points, bias=False)
+        self.__value = nn.Linear(in_features=in_embedding_dim, out_features=out_embedding_dim, bias=False)
+
+        # Attributes.
+        self.__offset_attention = None
+        self.__patch_size = patch_size
+
+        # Buffers.
+        self.register_buffer(name="reference_points", tensor=self.__get_reference_points(height=patch_size, width=patch_size))
+
+
+    # Properties.
+    @property
+    def offset_attention(self):
+        """
+        Get the offset attention matrix computed by the forward pass.
+
+        Returns:
+            torch.Tensor: The offset attention matrix.
+        """
+        return self.__offset_attention
+
+
+    # Methods.
+    def forward(self, query, value):
+        """
+        Forward pass of the deformable attention mechanism.
+
+        Args:
+            query (torch.Tensor): The query tensor, usually the text embeddings.
+            value (torch.Tensor): The value tensor, usually the image embeddings.
+
+        Returns:
+            torch.Tensor: The output tensor.
+        """
+        # Compute the attention weights.
+        offsets, attention_weights = self.__get_offsets_and_attention_weights(query=query)
+
+        # Apply the offsets to the reference points.
+        sampled_values = self.__apply_offsets_to_reference_points(
+            reference_points=self.reference_points,
+            offsets=offsets,
+            value=value
+        )
+
+        # Compute the output tensor.
+        output = (sampled_values * attention_weights[:, None, :, :]).sum(dim=-1)
+        output = output.permute(0, 2, 1)
+
+        return output
+
+
+    # Private methods.
+    def __get_offsets_and_attention_weights(self, query):
+        """
+        Computes the offsets and attention weights given the query tensor.
+
+        Args:
+            query (torch.Tensor): The query tensor, usually the text embeddings.
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: The offsets and attention weights.
+        """
+        # Get tensor shapes.
+        B, N, _ = query.size()
+
+        # Compute the attention weights.
+        attention_weights = self.__att_weight(query)
+        attention_weights = attention_weights.view(B, N, self.num_levels * self.num_points)
+        attention_weights = F.softmax(input=attention_weights, dim=-1)
+        attention_weights = attention_weights.view(B, N, self.num_levels, self.num_points)
+
+        # Compute the offsets.
+        offsets = self.__offset(query)
+        offsets = offsets.view(B, N, self.num_levels, self.num_points, 2)
+
+        return offsets, attention_weights
+
+
+    def __get_reference_points(self, height, width):
+        """
+        Computes the reference points given the height and width of the image.
+
+        Args:
+            height (int): The height of the image.
+            width (int): The width of the image.
+
+        Returns:
+            torch.Tensor: The reference points.
+        """
+        # Create the reference points.
+        ref_x = torch.linspace(start=0, end=1, steps=width)
+        ref_y = torch.linspace(start=0, end=1, steps=height)
+        ref_x, ref_y = torch.meshgrid(ref_x, ref_y)
+
+        # Reshape the reference points.
+        ref_x = ref_x.reshape(-1)[None]
+        ref_y = ref_y.reshape(-1)[None]
+
+        # Stack the reference points.
+        reference_points = torch.stack(tensors=[ref_x, ref_y], dim=-1)
+
+        return reference_points
+
+
+    def __apply_offsets_to_reference_points(self, reference_points, offsets, value):
+        """
+        Applies the offsets to the reference points.
+
+        Args:
+            reference_points (torch.Tensor): The reference points.
+            offsets (torch.Tensor): The offsets.
+            value (torch.Tensor): The value tensor, usually the image embeddings.
+
+        Returns:
+            torch.Tensor: The output tensor.
+        """
+        # Get tensor shapes.
+        B, N, _, _ = offsets.size()
+
+        # Compute the sampling locations.
+        sampling_locations = reference_points[:, :, None, :] + offsets
+        sampling_locations = (
+            sampling_locations / 
+            torch.tensor([self.__patch_size - 1, self.__patch_size - 1], device=value.device) - 
+            0.5
+        ) * 2
+
+        # Compute the sampled values.
+        v = self.__value(value)
+        v = v.view(B, -1, self.__patch_size, self.__patch_size)
+
+        sampled_values = F.grid_sample(
+            input=v,
+            grid=sampling_locations,
+            mode="bilinear",
+            align_corners=False
+        )
+
+        return sampled_values
 
 
 class MultiHeadAttention(nn.Module):
