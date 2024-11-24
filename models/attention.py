9d1d2f79e9cc306d799530ae4a1e20d8c8277911
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2024-11-24 19:07:34 -0300

    update: Add multi-head for deformable attention and fix some shape issues.

--- models/attention.py
+++ models/attention.py
@@ -114,7 +114,7 @@ class DeformableAttention(nn.Module):
 
 
     # Special methods.
-    def __init__(self, in_embedding_dim, out_embedding_dim, num_points, patch_size, num_levels = 1):
+    def __init__(self, in_embedding_dim, out_embedding_dim, num_points, patch_size):
         """
         Initializes the DeformableAttention class that computes the attention weights with a 
         deformable mechanism that uses an offset tensor from image embeddings used to deform the 
@@ -125,18 +125,18 @@ class DeformableAttention(nn.Module):
             out_embedding_dim (int): The output embedding dimension.
             num_points (int): The number of points to deform the attention weights.
             patch_size (int): The size of the patch used to compute the reference points.
-            num_levels (int): The number of levels to deform the attention weights. (Default: 1)
         """
         super().__init__()
 
         # Layers.
-        self.__offset = nn.Linear(in_features=in_embedding_dim, out_features=num_levels * num_points * 2, bias=False)
-        self.__att_weight = nn.Linear(in_features=in_embedding_dim, out_features=num_levels * num_points, bias=False)
+        self.__offset = nn.Linear(in_features=in_embedding_dim, out_features=num_points * 2, bias=False)
+        self.__att_weight = nn.Linear(in_features=in_embedding_dim, out_features=num_points, bias=False)
         self.__value = nn.Linear(in_features=in_embedding_dim, out_features=out_embedding_dim, bias=False)
 
         # Attributes.
         self.__offset_attention = None
         self.__patch_size = patch_size
+        self.__num_points = num_points
 
         # Buffers.
         self.register_buffer(name="reference_points", tensor=self.__get_reference_points(height=patch_size, width=patch_size))
@@ -199,13 +199,13 @@ class DeformableAttention(nn.Module):
 
         # Compute the attention weights.
         attention_weights = self.__att_weight(query)
-        attention_weights = attention_weights.view(B, N, self.num_levels * self.num_points)
+        attention_weights = attention_weights.view(B, N, self.__num_points)
         attention_weights = F.softmax(input=attention_weights, dim=-1)
-        attention_weights = attention_weights.view(B, N, self.num_levels, self.num_points)
+        attention_weights = attention_weights.view(B, N, self.__num_points)
 
         # Compute the offsets.
         offsets = self.__offset(query)
-        offsets = offsets.view(B, N, self.num_levels, self.num_points, 2)
+        offsets = offsets.view(B, N, self.__num_points, 2)
 
         return offsets, attention_weights
 
@@ -277,17 +277,14 @@ class MultiHeadAttention(nn.Module):
 
 
     # Special methods.
-    def __init__(self, num_heads, in_embedding_dim, attention_size = None):
+    def __init__(self, num_heads, in_embedding_dim):
         """
         Initializes the MultiHeadAttention class that computes the attention weights with the 
-        normal dot-product mechanism. Optionally, it can also compute the attention weights with a 
-        deformable mechanism that uses an offset tensor from image embeddings used to deform the 
-        attention weights on image based on both text and image embeddings.
+        normal dot-product mechanism.
 
         Args:
             num_heads (int): The number of heads.
             in_embedding_dim (int): The input embedding dimension.
-            attention_size (int): The size of the attention tensor. (Default: None)
         """
         super().__init__()
 
@@ -299,8 +296,7 @@ class MultiHeadAttention(nn.Module):
         self.__attention_heads = nn.ModuleList([
             Attention(
                 in_embedding_dim=in_embedding_dim, 
-                out_embedding_dim=head_embedding_dim, 
-                attention_size=attention_size
+                out_embedding_dim=head_embedding_dim
             ) for _ in range(num_heads)
         ])
 
@@ -319,6 +315,69 @@ class MultiHeadAttention(nn.Module):
         return {"head_%d" % i: head.attention for i, head in enumerate(self.__attention_heads)}
 
 
+    # Methods.
+    def forward(self, query, key, value):
+        """
+        Forward pass of the multi-head attention mechanism. Make sure to enable the offset layer 
+        defining the `attention_size` parameter in the constructor, otherwise it will raise an error 
+        if you pass the `offset` parameter during the forward pass.
+
+        Args:
+            query (torch.Tensor): The query tensor.
+            key (torch.Tensor): The key tensor.
+            value (torch.Tensor): The value tensor.
+
+        Returns:
+            torch.Tensor: The output tensor.
+        """
+        # Compute the attention heads.
+        attention_heads = [head(query, key, value) for head in self.__attention_heads]
+
+        # Concatenate the attention heads.
+        concatenated_attention_heads = torch.cat(attention_heads, dim=-1)
+
+        # Compute the output tensor.
+        output = self.__linear(concatenated_attention_heads)
+
+        return output
+
+
+class DeformableMultiHeadAttention(nn.Module):
+
+
+    # Special methods.
+    def __init__(self, num_heads, in_embedding_dim, num_points, patch_size):
+        """
+        Initializes the DeformableMultiHeadAttention class that computes the attention weights with 
+        a deformable mechanism that uses an offset tensor from image embeddings used to deform the 
+        attention weights on image based on both text and image embeddings.
+
+        Args:
+            num_heads (int): The number of heads.
+            in_embedding_dim (int): The input embedding dimension.
+            num_points (int): The number of points to deform the attention weights.
+            patch_size (int): The size of the patch used to compute the reference points.
+        """
+        super().__init__()
+
+        # Validate the number of heads.
+        assert in_embedding_dim % num_heads == 0, "The number of heads must be divisible by the input embedding dimension."
+        head_embedding_dim = in_embedding_dim // num_heads
+
+        # Layers.
+        self.__attention_heads = nn.ModuleList([
+            DeformableAttention(
+                in_embedding_dim=in_embedding_dim, 
+                out_embedding_dim=head_embedding_dim, 
+                num_points=num_points, 
+                patch_size=patch_size
+            ) for _ in range(num_heads)
+        ])
+
+        self.__linear = nn.Linear(in_features=in_embedding_dim, out_features=in_embedding_dim)
+
+
+    # Properties.
     @property
     def offset_attention(self):
         """
@@ -331,23 +390,19 @@ class MultiHeadAttention(nn.Module):
 
 
     # Methods.
-    def forward(self, query, key, value, offset = None):
+    def forward(self, query, value):
         """
-        Forward pass of the multi-head attention mechanism. Make sure to enable the offset layer 
-        defining the `attention_size` parameter in the constructor, otherwise it will raise an error 
-        if you pass the `offset` parameter during the forward pass.
+        Forward pass of the deformable multi-head attention mechanism.
 
         Args:
-            query (torch.Tensor): The query tensor.
-            key (torch.Tensor): The key tensor.
-            value (torch.Tensor): The value tensor.
-            offset (torch.Tensor): The offset tensor. (Default: None)
+            query (torch.Tensor): The query tensor, usually the text embeddings.
+            value (torch.Tensor): The value tensor, usually the image embeddings.
 
         Returns:
             torch.Tensor: The output tensor.
         """
         # Compute the attention heads.
-        attention_heads = [head(query, key, value, offset) for head in self.__attention_heads]
+        attention_heads = [head(query, value) for head in self.__attention_heads]
 
         # Concatenate the attention heads.
         concatenated_attention_heads = torch.cat(attention_heads, dim=-1)
