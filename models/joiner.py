673042f5970126665b1473ac05c8c660888fd764
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2025-02-09 11:10:29 -0300

    update: Convert joiner block into a transformer-like model.

--- models/joiner.py
+++ models/joiner.py
@@ -13,7 +13,7 @@ class PositionalEncoding(nn.Module):
     # Special methods.
     def __init__(
             self, 
-            high_feat_resolution = (40, 40), 
+            n_positions,
             emb_dim = 512
         ):
         """
@@ -21,22 +21,15 @@ class PositionalEncoding(nn.Module):
         with positional information.
 
         Args:
-            high_feat_resolution (tuple): Resolution of the high-level image feature. (Default: (40, 40))
-            emb_dim (int): Dimension of the embedding. (Default: 512)
+            n_positions (int): Number of positions to encode.
+            emb_dim (int): Dimension of the embeddings. (Default: 512)
         """
         super().__init__()
 
-        # Check if the embedding dimension is divisible 
-        # by 2.
-        if emb_dim % 2 != 0:
-            raise ValueError("Embedding dimension must be divisible by 2.")
-        half_dim = emb_dim // 2
-
-        col_pe_len, row_pe_len = high_feat_resolution
-
         # Layers.
-        self.col_emb = nn.Embedding(num_embeddings=col_pe_len, embedding_dim=half_dim)
-        self.row_emb = nn.Embedding(num_embeddings=row_pe_len, embedding_dim=half_dim)
+        self.pe = nn.Embedding(num_embeddings=n_positions, embedding_dim=emb_dim)
+
+        self.register_buffer(name="indices", tensor=torch.arange(n_positions))
 
 
     # Methods.
@@ -48,61 +41,229 @@ class PositionalEncoding(nn.Module):
             image_feature (torch.Tensor): Image feature tensor.
 
         Returns:
-            torch.Tensor: Positional encoding of the image feature tensor.
+            torch.Tensor: Encoded image feature tensor.
         """
         # Get shape of the image feature tensor.
         if not len(image_feature.size()) == 4:
             raise ValueError("Image feature tensor must be 4D.")
-        B, _, H, W = image_feature.size()
+        
+        # Flatten image feature tensor.
+        image_flatten = image_feature.flatten(2).permute(0, 2, 1)
 
-        # Compute range of positional encodings.
-        col_range = torch.arange(W, device=image_feature.device)
-        row_range = torch.arange(H, device=image_feature.device)
+        # Compute positional encoding.
+        pe = self.pe(self.indices).unsqueeze(0)
+        image_flatten = image_flatten + pe
 
-        # Project positional encodings.
-        col_emb = self.col_emb(col_range)
-        row_emb = self.row_emb(row_range)
+        return image_flatten
 
-        # Concatenate positional encodings.
-        pe = torch.cat(tensors=[
-            col_emb.unsqueeze(0).repeat(H, 1, 1), 
-            row_emb.unsqueeze(1).repeat(1, W, 1)
-        ], dim=-1)
 
-        # Reshape to match the image feature 
-        # size.
-        pe = pe.permute(2, 0, 1).unsqueeze(0).repeat(B, 1, 1, 1)
+class Attention(nn.Module):
 
-        return pe
+
+    def __init__(self, emb_proj, emb_dim = 512):
+        """
+        Attention layer used to compute attention scores and weights.
+
+        Args:
+            emb_proj (int): Projected dimension of the embeddings.
+            emb_dim (int): Dimension of the embeddings. (Default: 512)
+        """
+        super().__init__()
+
+        # Layers.
+        self.query = nn.Linear(in_features=emb_dim, out_features=emb_proj)
+        self.key = nn.Linear(in_features=emb_dim, out_features=emb_proj)
+        self.value = nn.Linear(in_features=emb_dim, out_features=emb_proj)
+
+
+    def forward(self, query, key, value):
+        """
+        Forward pass of the attention layer.
+
+        Args:
+            query (torch.Tensor): Query tensor.
+            key (torch.Tensor): Key tensor.
+            value (torch.Tensor): Value tensor.
+
+        Returns:
+            torch.Tensor: Attended value tensor.
+        """
+        # Project query, key and value tensors.
+        query = self.query(query)
+        key = self.key(key)
+        value = self.value(value)
+
+        # Compute attention scores.
+        attention_scores = query @ key.transpose(-2, -1)
+        attention_scores = attention_scores / (key.size(-1) ** 0.5)
+
+        # Compute attention weights.
+        attention_weights = torch.softmax(input=attention_scores, dim=-1)
+
+        # Compute attended value.
+        attended_value = attention_weights @ value
+
+        return attended_value
+
+
+class MultiHeadAttention(nn.Module):
+
+
+    def __init__(self, num_heads = 8, emb_dim = 512):
+        """
+        Multi-head attention layer used to compute multiple attention heads.
+
+        Args:
+            num_heads (int): Number of heads. (Default: 8)
+            emb_dim (int): Dimension of the embeddings. (Default: 512)
+        """
+        super().__init__()
+
+        # Check if the embedding dimension is divisible 
+        # by the number of heads.
+        if emb_dim % num_heads != 0:
+            raise ValueError("Embedding dimension must be divisible by the number of heads.")
+        emb_proj = emb_dim // num_heads
+
+        # Layers.
+        self.attention_heads = nn.ModuleList(modules=[
+            Attention(emb_proj=emb_proj, emb_dim=emb_dim) for _ in range(num_heads)
+        ])
+        self.projection = nn.Linear(in_features=emb_dim, out_features=emb_dim)
+
+
+    def forward(self, query, key, value):
+        """
+        Forward pass of the multi-head attention layer.
+
+        Args:
+            query (torch.Tensor): Query tensor.
+            key (torch.Tensor): Key tensor.
+            value (torch.Tensor): Value tensor.
+
+        Returns:
+            torch.Tensor: Multi-head attended value tensor.
+        """
+        # Compute attention heads.
+        attention_heads = [
+            attention_head(query, key, value) for attention_head in self.attention_heads
+        ]
+        attention_heads = torch.cat(tensors=attention_heads, dim=-1)
+
+        # Project attention heads.
+        projected_attention_heads = self.projection(attention_heads)
+
+        return projected_attention_heads
+
+
+class FeedForward(nn.Module):
+
+
+    def __init__(self, emb_dim = 512, ff_dim = 2048):
+        """
+        Feed-forward layer used to merge text and image embeddings.
+
+        Args:
+            emb_dim (int): Dimension of the embeddings. (Default: 512)
+            ff_dim (int): Dimension of the feed-forward layer. (Default: 2048)
+        """
+        super().__init__()
+
+        # Layers.
+        self.ff = nn.Sequential(
+            nn.Linear(in_features=emb_dim, out_features=ff_dim),
+            nn.ReLU(),
+            nn.Linear(in_features=ff_dim, out_features=emb_dim)
+        )
+
+
+    def forward(self, x):
+        """
+        Forward pass of the feed-forward layer.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+
+        Returns:
+            torch.Tensor: Output tensor.
+        """
+        return self.ff(x)
+
+
+
+class JoinerBlock(nn.Module):
+
+
+    # Special methods.
+    def __init__(self, emb_dim = 512, num_heads = 8, ff_dim = 2048):
+        """
+        Joiner block used to join text and image embeddings. It consists of a multi-head 
+        attention layer and a feed-forward layer used to merge both text and image embeddings.
+
+        Args:
+            emb_dim (int): Dimension of the embeddings. (Default: 512)
+            num_heads (int): Number of heads in the multi-head attention layer. (Default: 8)
+            ff_dim (int): Dimension of the feed-forward layer. (Default: 204
+        """
+        super().__init__()
+
+        # Layers.
+        self.multi_head_attention = MultiHeadAttention(num_heads=num_heads, emb_dim=emb_dim)
+        self.feed_forward = FeedForward(emb_dim=emb_dim, ff_dim=ff_dim)
+        self.norm1 = nn.LayerNorm(normalized_shape=emb_dim)
+        self.norm2 = nn.LayerNorm(normalized_shape=emb_dim)
+
+
+    # Methods.
+    def forward(self, text_embedding, image_embedding):
+        """
+        Forward pass of the joiner block.
+
+        Args:
+            text_embedding (torch.Tensor): Text embedding tensor. It will be used as the query tensor.
+            image_embedding (torch.Tensor): Image embedding tensor. It will be used as the key and value tensors.
+
+        Returns:
+            torch.Tensor: Output tensor.
+        """
+        # Compute multi-head attention.
+        attention_output = self.multi_head_attention(text_embedding, image_embedding, image_embedding)
+        attention_output = self.norm1(text_embedding + attention_output)
+
+        # Compute feed-forward layer.
+        feed_forward_output = self.feed_forward(attention_output)
+        feed_forward_output = self.norm2(attention_output + feed_forward_output)
+
+        return feed_forward_output
+
 
 
 class Joiner(nn.Module):
 
 
     # Special methods.
-    def __init__(self, *image_token_dim, emb_dim = 512, context_window_size = 64, high_feat_resolution = (40, 40)):
+    def __init__(self, image_tokens, emb_dim = 512, num_heads = 8, ff_dim = 2048, num_joins = 3):
         super().__init__()
 
         # Layers.
-        self.token_projection = nn.ModuleList(modules=[
-            nn.Linear(in_features=feat, out_features=context_window_size) for feat in image_token_dim
+        self.img_pe = nn.ModuleList(modules=[
+            PositionalEncoding(n_positions=image_token, emb_dim=emb_dim) for image_token in image_tokens
         ])
-        self.img_pe = PositionalEncoding(
-            high_feat_resolution=high_feat_resolution, 
-            emb_dim=emb_dim
-        )
+        self.joiner_blocks = nn.ModuleList(modules=[
+            JoinerBlock(emb_dim=emb_dim, num_heads=num_heads, ff_dim=ff_dim) for _ in range(num_joins)
+        ])
 
-        num_image_feature_levels = len(image_token_dim)
+        num_image_feature_levels = len(image_tokens)
         self.level_emb = nn.Parameter(data=torch.Tensor(num_image_feature_levels, emb_dim))
 
 
     # Methods.
-    def forward(self, *image_features, text_embedding):
+    def forward(self, image_features, text_embedding):
         """
         Forward pass of the joiner block.
 
         Args:
-            image_features (tuple): Tuple containing image embedding tensors.
+            image_features (list): List containing image embedding tensors.
             text_embedding (torch.Tensor): Text embedding tensor.
 
         Returns:
@@ -112,31 +273,23 @@ class Joiner(nn.Module):
         processed_image_features = []
         for level, image_feature in enumerate(iterable=image_features):
 
-            # Flatten feature.
-            flt_image_feature = image_feature.flatten(2)
-
-            # Compute positional encoding for the
-            # current feature map.
-            pe = self.img_pe(image_feature)
-            pe = pe.flatten(2)
+            # Compute positional encoding.
+            image_pe = self.img_pe[level](image_feature)
 
             # Compute level embedding.
-            level_emb = self.level_emb[level].view(1, -1, 1)
+            level_emb = self.level_emb[level].view(1, 1, -1)
 
-            # Add level embedding and positional encoding 
-            # to the feature map.
-            flt_image_feature = flt_image_feature + pe + level_emb
+            # Add level embedding to the feature map.
+            feature = image_pe + level_emb
 
-            processed_image_features.append(flt_image_feature)
+            print(feature.size())
+            processed_image_features.append(feature)
 
         # Project image embeddings.
-        projected_image_embeddings = [
-            projection(image_embedding) for projection, image_embedding in zip(self.token_projection, processed_image_features)
-        ]
-        projected_image_embeddings = torch.cat(projected_image_embeddings, dim=-1)
-        projected_image_embeddings = projected_image_embeddings.permute(0, 2, 1)
+        processed_image_features = torch.cat(processed_image_features, dim=1)
 
-        # Concatenate text and image embeddings.
-        joined_embedding = torch.cat(tensors=[text_embedding, projected_image_embeddings], dim=1)
+        # Join text and image embeddings.
+        for joiner_block in self.joiner_blocks:
+            text_embedding = joiner_block(text_embedding, processed_image_features)
 
-        return joined_embedding
+        return text_embedding
