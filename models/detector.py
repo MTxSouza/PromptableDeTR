b29efd1cd5a1e7e8748289e1f503e4f25b67f6cc
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2025-02-20 20:15:08 -0300

    fix: Add missing layer before the bbox and score projection.

--- models/detector.py
+++ models/detector.py
@@ -39,4 +39,33 @@ class PromptableDeTR(BasePromptableDeTR):
 
     # Methods.
     def forward(self, image, prompt):
-        pass
+        """
+        Forward pass of the detector.
+
+        Args:
+            image (torch.Tensor): Image tensor.
+            prompt (torch.Tensor): Prompt tensor.
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: Bounding box and presence predictions.
+        """
+        logger.info(msg="Calling `Detector` forward method.")
+        logger.debug(msg="- Image shape: %s" % (image.shape,))
+        logger.debug(msg="- Prompt shape: %s" % (prompt.shape,))
+
+        # Compute joint embeddings.
+        logger.debug(msg="- Calling `BasePromptableDeTR` forward method.")
+        joint = super().forward(image=image, prompt=prompt)
+        logger.debug(msg="- Result of the `BasePromptableDeTR` forward method: %s." % (joint.shape,))
+
+        # Predict bounding boxes and presence.
+        logger.debug(msg="- Calling `nn.Sequential` block to the tensor %s." % (joint.shape,))
+        out = self.detector(joint)
+        logger.debug(msg="- Result of the `nn.Sequential` block: %s." % (out.shape,))
+
+        logger.debug(msg="- Calling `nn.Linear` block to the tensor %s." % (out.shape,))
+        bbox = self.bbox_predictor(out)
+        presence = self.presence_predictor(out)
+        logger.debug(msg="- Result of the `nn.Linear` block: %s and %s." % (bbox.shape, presence.shape))
+
+        return bbox, presence
