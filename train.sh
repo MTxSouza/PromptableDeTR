d26f2c95181ecfeb9fab05a73d69c13459d23f19
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2025-06-14 19:49:52 -0300

    update: Set new parameters in bash script.

--- train.sh
+++ train.sh
@@ -7,9 +7,9 @@
 SEED=${SEED:-"42"}
 
 # Dataset params.
-DATASET_DIR=${DATASET_DIR}
+TRAIN_DATASET_DIR=${DATASET_DIR}
+VALID_DATASET_DIR=${VALID_DATASET_DIR:-"${DATASET_DIR}"}
 IMAGE_DIR=${IMAGE_DIR}
-VALID_SPLIT=${VALID_SPLIT:-"0.2"}
 MASK_RATIO=${MASK_RATIO:-"0.8"}
 
 # Model params.
@@ -35,9 +35,9 @@ L1_WEIGHT=${L1_WEIGHT:-"1.0"}
 
 python install_utilities.py
 python train.py \
-        --dataset-dir $DATASET_DIR \
+        --train-dataset-dir $DATASET_DIR \
+        --valid-dataset-dir $VALID_DATASET_DIR \
         --image-dir $IMAGE_DIR \
-        --valid-split $VALID_SPLIT \
         --shuffle \
         --vocab-file $VOCAB_FILE \
         --imgw $IMG_ENC_WEIGHT \
