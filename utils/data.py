7497f963e11765b41c6a47cf8fb162d8e51695c7
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2025-07-27 15:25:50 -0300

    create: Add function to convert xywh to xyxy.

--- utils/data.py
+++ utils/data.py
@@ -129,3 +129,35 @@ def box_xyxy_to_cxcywh(boxes, width, height):
         cxcywh = torch.cat([class_col.unsqueeze(-1), cxcywh], dim=-1)
 
     return cxcywh
+
+
+def xywh_to_xyxy(boxes, width=640, height=640):
+    """
+    Converts bounding boxes from (x_min, y_min, width, height) to 
+    (x_min, y_min, x_max, y_max) format.
+
+    Args:
+        boxes (torch.Tensor): The bounding boxes with shape (B, N, 4).
+        width (int): The width of the image. (Default: 640)
+        height (int): The height of the image. (Default: 640)
+    
+    Returns:
+        torch.Tensor: The bounding boxes in (x_min, y_min, x_max, y_max) format.
+    """
+    # Check if the batch channel is present.
+    if boxes.dim() == 2:
+        boxes = boxes.unsqueeze(dim=0)
+
+    # Convert 0-1 coordinates to pixel coordinates.
+    new_boxes = boxes.clone()
+    new_boxes[:, :, 0::2] *= width
+    new_boxes[:, :, 1::2] *= height
+
+    new_boxes[:, :, 2] = new_boxes[:, :, 0] + new_boxes[:, :, 2]
+    new_boxes[:, :, 3] = new_boxes[:, :, 1] + new_boxes[:, :, 3]
+
+    # Clamp the bounding box coordinates.
+    new_boxes[:, :, 0::2] = torch.clamp(new_boxes[:, :, 0::2], min=0, max=width)
+    new_boxes[:, :, 1::2] = torch.clamp(new_boxes[:, :, 1::2], min=0, max=height)
+
+    return new_boxes
