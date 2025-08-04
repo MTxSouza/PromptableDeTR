6286e68dc62bbecd982f167ddb33e195d2859d2c
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2025-08-04 05:15:12 -0300

    fix: Organize code and fix issue to compute boxes.

--- utils/data.py
+++ utils/data.py
@@ -3,6 +3,7 @@ This module contains useful functions to manipulate any data type and objects
 in the PromptVision project.
 """
 import torch
+from torchvision.ops.boxes import box_area
 
 
 # Functions.
@@ -38,3 +39,67 @@ def xywh_to_xyxy(boxes, width=640, height=640):
     new_boxes = new_boxes.numpy()
 
     return new_boxes
+
+
+def xywh2xyxy(boxes):
+    """
+    This function converts the coordinates from (x, y, w, h) to (x1, y1, x2, y2).
+
+    Args:
+        boxes (torch.Tensor): The boxes with shape (N, 4).
+
+    Returns:
+        torch.Tensor: The converted boxes with shape (N, 4).
+    """
+    cx, cy, w, h = boxes.unbind(dim=-1)
+    x1 = cx - w / 2
+    y1 = cy - h / 2
+    x2 = cx + w / 2
+    y2 = cy + h / 2
+    new_boxes = torch.stack([x1, y1, x2, y2], dim=-1)
+    return new_boxes
+
+
+def generalized_iou(boxes1, boxes2):
+    """
+    Compute the Generalized Intersection over Union (GIoU) between two sets of boxes.
+
+    Args:
+        boxes1 (torch.Tensor): The first set of boxes with shape (N, 4).
+        boxes2 (torch.Tensor): The second set of boxes with shape (M, 4).
+
+    Returns:
+        torch.Tensor: The GIoU between the two sets of boxes.
+    """
+    # Convert coordinates to (x1, y1, x2, y2).
+    boxes1 = xywh_to_xyxy(boxes=boxes1)
+    boxes2 = xywh_to_xyxy(boxes=boxes2)
+
+    assert (boxes1[:, 2:] >= boxes1[:, :2]).all()
+    assert (boxes2[:, 2:] >= boxes2[:, :2]).all()
+
+    # Compute area.
+    area1 = box_area(boxes1)
+    area2 = box_area(boxes2)
+
+    # Compute intersection.
+    lt = torch.max(boxes1[:, None, :2], boxes2[:, :2])
+    rb = torch.min(boxes1[:, None, 2:], boxes2[:, 2:])
+    wh = (rb - lt).clamp(min=0)
+    inter = wh[:, :, 0] * wh[:, :, 1]
+
+    # Compute union.
+    union = area1[:, None] + area2 - inter
+
+    # Compute IoU.
+    iou = inter / union
+
+    # Compute GIoU.
+    lt = torch.min(boxes1[:, None, :2], boxes2[:, :2])
+    rb = torch.max(boxes1[:, None, 2:], boxes2[:, 2:])
+    wh = (rb - lt).clamp(min=0)
+    area = wh[:, :, 0] * wh[:, :, 1]
+
+    giou = iou - (area - union) / area
+
+    return giou
